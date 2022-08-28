8eaf60257a4c36fe5f277e702b033c9f0d3dad91
Pedro Calcado <pecalcado@gmail.com>
2022-08-28 12:43:15 -0300

    versão0

---
+++ script.js
@@ -0,0 +1,11 @@
+const usuario = prompt('Qual seu lindo nome?')
+
+
+// function pegarMensagen(){
+//     let mensagens = [];
+//     let response = axio.get()
+    
+// }
+// function postarMensagens(){
+
+// }
