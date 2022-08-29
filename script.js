3ff7c001e66d6cc76890eda55c0690d806cab543
Pedro Calcado <pecalcado@gmail.com>
2022-08-29 16:39:33 -0300

    quaselá

--- script.js
+++ script.js
@@ -1,106 +1,160 @@
-let nome = { name:""}
-let mensagem = {from: "",
-to:"",   
- text: "",
- type: "",
-time: ""
-}
+let nomeUser = {};
+let nome;
 
+// let ultimaMensagem = document.querySelector('ul').lastChild;
 
-let ultimaMensagem = document.querySelector('ul').lastChild;
-let nomeUser = prompt('Qual seu lindo nome?');
-
-function enviarNome(nome){
-    let nome = {name: nomeUser};
-    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome);
-    requisicao.then(logou);
+function enviarNome(){
+    const participantes = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    nome = prompt('Qual seu lindo nome?');
+    console.log(nome)
+    let nomeUser = {name: nome};
+    const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeUser);
+    
+    requisicao.then(manterConexao);
     requisicao.catch(resolverProblema);
+    console.log(nomeUser)
 }
 
-function logou(){
-    renderizarMensagens();
-    setInterval(manterConexão, 5000);
-    setInterval(renderizarMensagens,3000 );
+enviarNome();
+setInterval(manterConexao, 5000)
+
+function manterConexao(){
+let conexao = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nome);
 
 }
-
-function manterConexão()
-let conexão = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", nome);
-
 function pegarMensagem(){
-    
-    const resposta = axio.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    resposta.then(mostrarMensagens);
-    resposta.catch(resolverProblema);
+    const resposta = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    resposta.then(renderizarMensagens);
     
 }
-function renderizarMensagens(resposta){
-    const mensagens = resposta.data
+pegarMensagem();
+
+setInterval(pegarMensagem, 3000)
+function renderizarMensagens(mensagens){
+   
     const ulmensagens = document.querySelector('ul');
     ulmensagens.innerHTML = '';
-    
-    
-    for(let i = 0; i < mensanges.lenght; i++){
-        if(mensagens[i].type === "status"){
-            const remetente = mensagens[i].from;
-            const texto = mensagens[i].text;
-            const hora = mensagens[i].time; 
-              
-            ulMensagens.innerHTML += `<li class = "status" data-identifier="message">
-            <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> <span class = "text">${texto}</span></p>
-        </li>`
-         }
-         if(mensagens[i].type === "message"){
-            const remetente = mensagens[i].from;
-            const texto = mensagens[i].text;
-            const hora = mensagens[i].time;
-            const destinatario = mensagens[i].to;
-         
-         ulMensagens.innerHTML += `<li class = "message" data-identifier="message">
-           <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> para <span class = "to"> ${destinatario}:</span> <span class = "text">${texto}</span></p>
-        </li>`
-         }
-         if(mensagens[i].type === "private_message"){
-            const remetente = mensagens[i].from;
-            const texto = mensagens[i].text;
-            const hora = mensagens[i].time;
-            const destinatario = mensagens[i].to;
-            if(nome.name === destinatario || nome.name === remetente || mensagens[i].to === "Todos"){
-               ulMensagens.innerHTML += `<li class = "private_message" data-identifier="message">
-               <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> reservadamente para <span class = "to"> ${destinatario}:</span> <span class = "text">${texto}</span></p>
-               </li>`
-            }
-        
-         }
-      }
-      const ultimaMensagem = document.querySelector('ul').lastChild;
-         if(ultimaMensagem.innerHTML !== ultimaMensagem.innerHTML){
-         ultimaMensagem = document.querySelector('ul').lastChild;
-         ultimaMensagem.scrollIntoView();
-      }
-   } 
+    for (i = 0; i < mensagens.data.length; i++ ){
+       ulmensagens.innerHTML += `<li class = "${mensagens.data[i].type}">
+      <p><span class = "time"> (${mensagens.data[i].time})</span> <span class = "from"> ${mensagens.data[i].from} </span> <span class = "text">${mensagens.data[i].text}</span></p></li>`
+    }
+   }
+let objeto;
+function enviarMensagem(){
+   let texto = document.querySelector("input").value;
+   objeto = {
+      from: nome,
+      to: "todos",
+      text: texto,
+      type: "message", 
+   }
+   let promessaEnvio = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objeto);
+   promessaEnvio.then(renderizarMinhaMensagem);
+   promessaEnvio.catch(resolverProblema);
+}
+
+function renderizarMinhaMensagem(){
+   const ulMensagens = document.querySelector('ul');
+   minhaMensagem = `<li class="message"> <span> ${objeto.nome} </span>
+   <span>${objeto.to}</span>
+   <span>${objeto.texto}</span>
+   </li>`;
+   
+   ulMensagens += minhaMensagem;
+   
+}
+console.log()
+// function atualizarChat(){
+
+//    const ultimaMensagem = document.querySelector('.ul').lastChild;
+//    ultimaMensagem.scrollIntoView();
+// }
+// atualizarChat()
+
 
 function resolverProblema(erro){
-    alert("Por favor, escolher outro nome")
-    window.location.reload()
+          alert("Por favor, escolher outro nome");
+          enviarNome();
+      
+      }
 
-}
-function enviarMensagem(this){
+      // function enviarMensagem(this){
     
-    const mensagemInput = document.querySelector(".divMensagem input");
-    mensagem.from = nome.name;
-    mensagem.text = mensagemInput.value;
-    mensagemInput.value = "";
- 
-    if(mensagem.to === ""){
-       mensagem.to = "Todos";
-    }
-    if(mensagem.type === ""){
-       mensagem.type = "message";
-    }
- 
-    const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagem);
-    promessa.then(carregarMensagens);
-    promessa.catch(tratarErroEnvio);
-    }
+      //        const mensagemInput = document.querySelector(".divMensagem input");
+      //        mensagem.from = nome.name;
+      //        mensagem.text = mensagemInput.value;
+      //        mensagemInput.value = "";
+          
+      //        if(mensagem.to === ""){
+      //           mensagem.to = "Todos";
+      //        }
+      //        if(mensagem.type === ""){
+      //           mensagem.type = "message";
+      //        }
+          
+      //        const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", mensagem);
+      //        promessa.then(carregarMensagens);
+      //        promessa.catch(tratarErroEnvio);
+      //        }
+
+// function logou(){
+//     renderizarMensagens();
+//     setInterval(manterConexão, 5000);
+//     setInterval(renderizarMensagens,3000 );
+
+// }
+
+
+
+
+    
+// }
+// function renderizarMensagens(resposta){
+//     const mensagens = resposta.data
+//     const ulmensagens = document.querySelector('ul');
+//     ulmensagens.innerHTML = '';
+    
+    
+//     for(let i = 0; i < mensanges.lenght; i++){
+//         if(mensagens[i].type === "status"){
+//             const remetente = mensagens[i].from;
+//             const texto = mensagens[i].text;
+//             const hora = mensagens[i].time; 
+              
+//             ulMensagens.innerHTML += `<li class = "status" data-identifier="message">
+//             <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> <span class = "text">${texto}</span></p>
+//         </li>`
+//          }
+//          if(mensagens[i].type === "message"){
+//             const remetente = mensagens[i].from;
+//             const texto = mensagens[i].text;
+//             const hora = mensagens[i].time;
+//             const destinatario = mensagens[i].to;
+         
+//          ulMensagens.innerHTML += `<li class = "message" data-identifier="message">
+//            <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> para <span class = "to"> ${destinatario}:</span> <span class = "text">${texto}</span></p>
+//         </li>`
+//          }
+//          if(mensagens[i].type === "private_message"){
+//             const remetente = mensagens[i].from;
+//             const texto = mensagens[i].text;
+//             const hora = mensagens[i].time;
+//             const destinatario = mensagens[i].to;
+//             if(nome.name === destinatario || nome.name === remetente || mensagens[i].to === "Todos"){
+//                ulMensagens.innerHTML += `<li class = "private_message" data-identifier="message">
+//                <p><span class = "time"> (${hora}) </span> <span class = "from"> ${remetente} </span> reservadamente para <span class = "to"> ${destinatario}:</span> <span class = "text">${texto}</span></p>
+//                </li>`
+//             }
+        
+//          }
+//       }
+//       const ultimaMensagem = document.querySelector('ul').lastChild;
+//          if(ultimaMensagem.innerHTML !== ultimaMensagem.innerHTML){
+//          ultimaMensagem = document.querySelector('ul').lastChild;
+//          ultimaMensagem.scrollIntoView();
+//       }
+//    } 
+
+// 
+//
  
